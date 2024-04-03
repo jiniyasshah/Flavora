4ca26b6b16df577526c973796ef82cc01a6dbba9
Jiniyas Shah <sanketshahts@gmail.com>
2024-04-03 14:08:42 +0545

    Fixed Menubar Mobile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           alt="hero-bg"
         />
 
-        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+        <div className="grid-container w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
           {heroData &&
             heroData.map((n) => (
               <div

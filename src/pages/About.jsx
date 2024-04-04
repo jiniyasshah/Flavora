37dbad1a22eee3fd5af42bd127bd1ea39aa2f89b
Jiniyas Shah <72019447+jiniyasshah@users.noreply.github.com>
2024-04-04 11:08:21 +0545

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
 
         <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
           About
-          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">Us</span>
+          <span className="text-[#ff531d] text-[3rem] lg:text-[5rem]">Us</span>
         </p>
 
         <p className="leading-8	 text-textColor text-center md:text-left md:w-[80%] text-lg">
